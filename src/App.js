840e8b4e9ad7154356081f8c722a4b4056e0b462
GuillermoFontana <guilleefontana@gmail.com>
2023-07-31 17:49:12 -0300

    iniciando con el esqueleto de react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import Error404 from 'containers/errors/Error404';
+import Home from 'containers/pages/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import store from 'store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <div>
-Hola Mundo!
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          {/* Error Display */}
+          <Route path="*" element={<Error404 />} />
+
+          {/* Home Display */}
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
